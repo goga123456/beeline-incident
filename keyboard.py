3113c2d9695cc3a0d50643b76a90abcc0a661e9d
goga123456 <110159417+goga123456@users.noreply.github.com>
2023-10-31 16:10:14 +0500

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -14,5 +14,6 @@ def get_p_or_v_kb() -> InlineKeyboardMarkup:
     b1 = InlineKeyboardButton('Фото', callback_data='btn_photo')
     b2 = InlineKeyboardButton('Видео', callback_data='btn_video')
     b3 = InlineKeyboardButton('Ни то ни другое', callback_data='btn_nothing')
-    kb2.add(b1, b2).add(b3)
+    b4 = InlineKeyboardButton('Назад', callback_data='Назад')
+    kb2.add(b1, b2).add(b3).add(b4)
     return kb2
