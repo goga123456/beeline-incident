4a29030858fde7a55d7cabbcd52b0cb33a73e9d2
goga123456 <110159417+goga123456@users.noreply.github.com>
2023-11-21 13:14:12 +0500

    Add files via upload

---
+++ kbs/inline_kbs.py
@@ -0,0 +1,27 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+def get_kb() -> InlineKeyboardMarkup:
+    kb = InlineKeyboardMarkup(resize_keyboard=True)
+    kb1 = InlineKeyboardButton('Horizon', callback_data='Horizon')
+    kb2 = InlineKeyboardButton('ClarifyCRM', callback_data='ClarifyCRM')
+    kb3 = InlineKeyboardButton('Проблемы с компьютером', callback_data='PK_problems')
+    kb4 = InlineKeyboardButton('Другое', callback_data='other')
+    kb.add(kb1, kb2).add(kb3, kb4)
+    return kb
+def ustraneno() -> InlineKeyboardMarkup:
+    kb = InlineKeyboardMarkup(resize_keyboard=True)
+    b1 = InlineKeyboardButton('Устранено', callback_data='Устранено')
+    b2 = InlineKeyboardButton('Не устранено', callback_data='Не устранено')
+    kb.add(b1, b2)
+    return kb
+def get_p_or_v_kb() -> InlineKeyboardMarkup:
+    kb2 = InlineKeyboardMarkup(resize_keyboard=True)
+    b1 = InlineKeyboardButton('Фото', callback_data='btn_photo')
+    b2 = InlineKeyboardButton('Видео', callback_data='btn_video')
+    b3 = InlineKeyboardButton('Ни то ни другое', callback_data='btn_nothing')
+    b4 = InlineKeyboardButton('Назад', callback_data='Назад')
+    kb2.add(b1, b2).add(b3).add(b4)
+    return kb2
+
+
