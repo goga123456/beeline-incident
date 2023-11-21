663d75611b137564b0c80feb5255b7656c287ee9
goga123456 <110159417+goga123456@users.noreply.github.com>
2023-11-21 13:13:47 +0500

    Delete ReplyKeyboard.py

--- ReplyKeyboard.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-
-def get_start_and_back_kb() -> ReplyKeyboardMarkup:
-    kmain = ReplyKeyboardMarkup(resize_keyboard=True)
-    b1 = KeyboardButton('🔙')
-    b2 = KeyboardButton('/start')
-    kmain.add(b1, b2)
-    return kmain
